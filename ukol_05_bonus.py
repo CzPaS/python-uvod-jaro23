269b5a5b47e9acd373eea37c0f48e4bb22106f4c
Pavla Slabihoudková <slabihoudkova@gmail.com>
2023-04-27 17:32:31 +0200

    bonus z 5. lekce

---
+++ ukol_05_bonus.py
@@ -0,0 +1,12 @@
+teploty = [
+    ["pondělí", 2.1, 5.2, 6.1, -0.1],
+    ["úterý", 2.2, 4.8, 5.6, -1.0],
+    ["středa", 3.3, 6.5, 5.9, 1.2],
+    ["čtvrtek", 2.9, 5.6, 6.0, 0.0],
+    ["pátek", 2.0, 4.6, 5.5, -1.2],
+    ["sobota", 1.0, 3.2, 2.1, -2.0],
+    ["neděle", 0.4, 2.7, 1.3, -2.8]
+]
+
+denni_prumer = {teplota[0] : round(sum(teplota[1:5])/len(teplota[1:5]), 2) for teplota in teploty}
+print(denni_prumer)
